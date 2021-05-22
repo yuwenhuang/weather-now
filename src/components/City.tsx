9f4b8bb992b2702c46909fb5713f255e55408201
Yu-Wen Huang <yuwenhuang813@gmail.com>
2021-05-22 13:00:17 +0200

    feat: add user input and render dashboard with city temperature

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -10,44 +10,49 @@ interface ICityWeather {
     }
 }
 
-const City =  () => {
-    const [error, setError] = useState<IError | null>(null);;
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [cityWeather, setCityWeather] = useState<ICityWeather | null>(null);
+interface CityProps {
+  name: string
+}
+
+const City =  (props: CityProps) => {
+  const { name } = props;
+  const [error, setError] = useState<IError | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [cityWeather, setCityWeather] = useState<ICityWeather | null>(null);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${API_KEY}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setIsLoaded(true);
+          setCityWeather(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
   
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect(() => {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${API_KEY}`)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            console.log(result)
-            setIsLoaded(true);
-            setCityWeather(result);
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }, [])
-  
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <ul>
-          {cityWeather?.main.temp}
-        </ul>
-      );
-    }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {name}: {cityWeather?.main?.temp} °C
+      </ul>
+    );
+  }
   }
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import City from './components/City';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <City />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const defaultCitys = ["London", "Berlin"]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+interface DashboardState {
+  userLocation: string;
+  isSubmitted: boolean;
+}
+
+class Dashboard extends Component<{}, DashboardState> {
+  state = {
+    userLocation: "",
+    isSubmitted: false,
+  };
+
+  handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    this.setState({isSubmitted: true})
+  }
+  handleInputChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    this.setState({userLocation: event.currentTarget.value})
+  }
+
+  renderUserInput = () => {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>Enter your city:</label>
+        <input type="text" value={this.state.userLocation} onChange={this.handleInputChange}></input>
+        <input type="submit" value="Submit" />
+      </form>)
+  }
+
+  render() {
+    const { isSubmitted, userLocation } = this.state
+    
+    return(
+      <React.StrictMode>
+        Dashboard
+      {this.renderUserInput()}
+      { isSubmitted && <City name={userLocation}/> }
+      <City name={defaultCitys[0]}/>
+      <City name={defaultCitys[1]}/>
+      </React.StrictMode>
+    )  
+  }
+}
+
+export default Dashboard
+
+ReactDOM.render(< Dashboard />, document.getElementById('root'));
